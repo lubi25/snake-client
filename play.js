6e10afd41c929b1c780ddfdbc40df91ac4c4e5e8
Lubi Islam <lubaba.islam91@gmail.com>
2024-03-03 14:41:21 -0500

    Shorten with ES6 syntax

--- play.js
+++ play.js
@@ -1,28 +1,3 @@
-const net = require("net");
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-
-  conn.setEncoding("utf8");
-
-  conn.on('connect', () => {
-    console.log('Successfully connected to game server');
-  });
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  conn.on('error', (err) => {
-    console.error('Connection error:', err);
-  });
-
-  return conn;
-};
+const connect = require('./client');
 
 connect();
-
-console.log("Starting client script...");

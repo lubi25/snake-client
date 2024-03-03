7a7d9ad7d071b438ca78937b4424429ab2dee81f
Lubi Islam <lubaba.islam91@gmail.com>
2024-03-03 18:49:04 -0500

    Add variables for constants

--- constants.js
+++ constants.js
@@ -1,7 +1,23 @@
 const IP = "localhost";
 const PORT = 50541;
+let player = process.argv[2] || "???";
+const moveUp = "Move: up";
+const moveDown = "Move: down";
+const moveLeft = "Move: left";
+const moveRight = "Move: right";
+const sayHowAreYou = "Say: How are you?";
+const saySlither = "Say: Time to slither!";
+const sayRace = "Say: Let's race!";
 
 module.exports = {
   IP,
   PORT,
+  player,
+  moveUp,
+  moveDown,
+  moveLeft,
+  moveRight,
+  sayHowAreYou,
+  saySlither,
+  sayRace,
 };

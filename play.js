e10ed710dd7396c39950da9a6cb07a8764f6092f
Lubi Islam <lubaba.islam91@gmail.com>
2024-03-03 14:19:23 -0500

    Add initial code

---
+++ play.js
@@ -0,0 +1,28 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+  });
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  conn.on('error', (err) => {
+    console.error('Connection error:', err);
+  });
+
+  return conn;
+};
+
+connect();
+
+console.log("Starting client script...");

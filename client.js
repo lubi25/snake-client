eaac154d9d3f5935dc84082c4ab0eab65a09eeef
Lubi Islam <lubaba.islam91@gmail.com>
2024-03-03 14:40:55 -0500

    Copy over and export connect function from play.js

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+  });
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  conn.on('error', (err) => {
+    console.error('Connection error:', err);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
